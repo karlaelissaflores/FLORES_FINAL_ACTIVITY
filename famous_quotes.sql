48ab7b37fcee7f4c6ee9d352dc37bb6019ea7025
Karla Elissa Flores <201880094@psu.palawan.edu.ph>
2021-06-10 02:37:25 +0800

    Create famous_quotes.sql

---
+++ famous_quotes.sql
@@ -0,0 +1,214 @@
+CREATE TABLE `fam_quotes` ( `id` int(10) unsigned NOT NULL auto_increment, `author` varchar(255) NOT NULL default '', `quote` mediumtext NOT NULL, PRIMARY KEY (`id`) ) ENGINE=MyISAM;
+
+INSERT INTO `fam_quotes` VALUES (1, 'Lee Iacocca', 'Motivation is everything. You can do the work of two people, but you can\'t be two people. Instead you have to inspire the next guy down the line AND get him to inspire his people.');
+
+INSERT INTO `fam_quotes` VALUES (2, 'Lee Iacocca', 'In a completely rational society, the best of us would be teachers AND the rest of us would have to settle for something else.');
+
+INSERT INTO `fam_quotes` VALUES (3, 'Lee Iacocca', 'Do you want me to tell you something really subversive Love is everything it\'s cracked up to be. That\'s why people are so cynical about it. . . . It really is worth fighting for, being brave for, risking everything for. AND the trouble is, if you don\'t risk anything, you risk even more.');
+
+INSERT INTO `fam_quotes` VALUES (4, 'Lee Iacocca', 'Management is nothing more than motivating other people.');
+
+INSERT INTO `fam_quotes` VALUES (5, 'Lee Iacocca', 'No matter what you\'ve done for yourself or for humanity, if you can\'t look back on
+HAVING given love AND attention to your own family, what have you really accomplished');
+
+INSERT INTO `fam_quotes` VALUES (6, 'Lee Iacocca', 'My father always used to say that WHEN you die, if you\'ve got five real friends, THEN you\'ve had a great life.');
+
+INSERT INTO `fam_quotes` VALUES (7, 'Lee Iacocca', 'You can have brilliant ideas, but if you can\'t get them across, your ideas won\'t get you anywhere.');
+
+INSERT INTO `fam_quotes` VALUES (8, 'Lee Iacocca', 'Incompetency begets incompetency. The last thing a guy who isn\'t sure of himself wants is a guy backing him up who is sure of himself.');
+
+INSERT INTO `fam_quotes` VALUES (9, 'Lee Iacocca', 'A true friend is the greatest of all blessings, AND that which we take the least care of all to acquire.');
+
+INSERT INTO `fam_quotes` VALUES (10, 'Lee Iacocca', 'People want economy AND they will pay any price to get it.');
+
+INSERT INTO `fam_quotes` VALUES (11, 'Lee Iacocca', 'The trick is to make sure you don\'t die waiting for prosperity to come.');
+
+INSERT INTO `fam_quotes` VALUES (12, 'H.L. Mencken', 'The whole aim of practical politics is to keep the populace IN a continual state of alarm (and hence clamorous to be led to safety) by menacing them with an endless series of hobgoblins, all of them imaginary.');
+
+INSERT INTO `fam_quotes` VALUES (13, 'H.L. Mencken', 'The penalty for laughing IN a courtroom is six months IN jail if it were not for this penalty, the jury would never hear the evidence.');
+
+INSERT INTO `fam_quotes` VALUES (14, 'H.L. Mencken', 'Imagine the Creator AS a stand up commedian - AND at once the world becomes explicable.');
+
+INSERT INTO `fam_quotes` VALUES (15, 'H.L. Mencken', 'We must respect the other fellow\'s religion,but only IN the sense AND to the extent that we respect his theory that his wife is beautiful AND his children smart.');
+
+INSERT INTO `fam_quotes` VALUES (16, 'H.L. Mencken', 'It is hard to believe that a man is telling the truth WHEN you know that you would lie if you were IN his place.');
+
+INSERT INTO `fam_quotes` VALUES (17, 'H.L. Mencken', 'Communism, like any other revealed religion, is largely made up of prophecies.');
+
+INSERT INTO `fam_quotes` VALUES (18, 'H.L. Mencken', 'JUDGE, n A law student who marks his own papers.');
+
+INSERT INTO `fam_quotes` VALUES (19, 'H.L. Mencken', 'A celebrity is one who is known to many persons he is glad he doesn\'t know.');
+
+INSERT INTO `fam_quotes` VALUES (20, 'H.L. Mencken', 'Any man who afflicts the human race with ideas must be prepared to see them misunderstood.');
+
+INSERT INTO `fam_quotes` VALUES (21, 'H.L. Mencken', 'A cynic is a man who, WHEN he smells flowers, looks around for a coffin.');
+
+INSERT INTO `fam_quotes` VALUES (22, 'H.L. Mencken', 'A home is not a mere transient shelter its essence lies IN the personalities of the people who live IN it.');
+
+INSERT INTO `fam_quotes` VALUES (23, 'H.L. Mencken', 'All men are frauds. The only difference BETWEEN them is that some admit it. I myself deny it.');
+
+INSERT INTO `fam_quotes` VALUES (24, 'H.L. Mencken', 'All successful newspapers are ceaselessly querulous AND bellicose. They never defend anyone or anything if they can help it if the job is forced
+ON them, they tackle it by denouncing someone or something else.');
+
+INSERT INTO `fam_quotes` VALUES (25, 'H.L. Mencken', 'All zoos actually offer to the public IN return for the taxes spent upon them is a form of idle AND witless amusement, compared to which a visit to a penitentiary, or even to a State legislature IN session, is informing, stimulating AND ennobling.');
+
+INSERT INTO `fam_quotes` VALUES (26, 'H.L. Mencken', 'A poet more than thirty years old is simply an overgrown child.');
+
+INSERT INTO `fam_quotes` VALUES (27, 'H.L. Mencken', 'An idealist is one who,
+ON noticing that a rose smells better than a cabbage, concludes that it will also make better soup.');
+
+INSERT INTO `fam_quotes` VALUES (28, 'H.L. Mencken', 'Giving every man a vote has no more made men wise AND free than Christianity has made them good.');
+
+INSERT INTO `fam_quotes` VALUES (29, 'H.L. Mencken', 'Democracy is the theory that the common people know what they want AND deserve to get it good AND hard.');
+
+INSERT INTO `fam_quotes` VALUES (30, 'H.L. Mencken', 'Every decent man is ashamed of the government he lives under.');
+
+INSERT INTO `fam_quotes` VALUES (31, 'H.L. Mencken', 'I never lecture, not because I am shy or a bad speaker, but simply because I detest the sort of people who go to lectures AND don\'t want to meet them.');
+
+INSERT INTO `fam_quotes` VALUES (32, 'H.L. Mencken', 'I believe that all government is evil, AND that trying to improve it is largely a waste of time.');
+
+INSERT INTO `fam_quotes` VALUES (33, 'H.L. Mencken', 'Criticism is prejudice made plausible.');
+
+INSERT INTO `fam_quotes` VALUES (34, 'H.L. Mencken', 'Faith may be defined briefly AS an illogical belief IN the occurrence of the improbable.');
+
+INSERT INTO `fam_quotes` VALUES (35, 'H.L. Mencken', 'For it is mutual trust, even more than mutual interest that holds human associations together. Our friends seldom profit us but they make us feel safe... Marriage is a scheme to accomplish exactly that same end.');
+
+INSERT INTO `fam_quotes` VALUES (36, 'H.L. Mencken', 'In the United States, doing good has come to be, like patriotism, a favorite device of persons with something to sell.');
+
+INSERT INTO `fam_quotes` VALUES (37, 'H.L. Mencken', 'For centuries, theologians have been explaining the unknowable IN terms of the-not-worth-knowing.');
+
+INSERT INTO `fam_quotes` VALUES (38, 'H.L. Mencken', 'Misogynist A man who hates women AS much AS women hate one another.');
+
+INSERT INTO `fam_quotes` VALUES (39, 'H.L. Mencken', 'It is inaccurate to say that I hate everything. I am strongly IN favor of common sense, common honesty, AND common decency. This makes me forever ineligible for public office.');
+
+INSERT INTO `fam_quotes` VALUES (40, 'H.L. Mencken', 'Men are the only animals that devote themselves, day IN AND day out, to making one another unhappy. It is an art like any other. Its virtuosi are called altruists.');
+
+INSERT INTO `fam_quotes` VALUES (41, 'H.L. Mencken', 'It is now quite lawful for a Catholic woman to avoid pregnancy by a resort to mathematics, though she is still forbidden to resort to physics or chemistry.');
+
+INSERT INTO `fam_quotes` VALUES (42, 'H.L. Mencken', 'Man is never honestly the fatalist, nor even the stoic. He fights his fate, often desperately. He is forever entering bold exceptions to the rulings of the bench of gods. This fighting, no doubt, makes for human progress, for it favors the strong AND the brave. It also makes for beauty, for lesser men try to escape
+FROM a hopeless AND intolerable world by creating a more lovely one of their own.');
+
+INSERT INTO `fam_quotes` VALUES (43, 'H.L. Mencken', 'It is impossible to imagine Goethe or Beethoven being good at billiards or golf.');
+
+INSERT INTO `fam_quotes` VALUES (44, 'H.L. Mencken', 'Love is the triumph of imagination over intelligence.');
+
+INSERT INTO `fam_quotes` VALUES (45, 'H.L. Mencken', 'It is even harder for the average ape to believe that he has descended
+FROM man.');
+
+INSERT INTO `fam_quotes` VALUES (46, 'H.L. Mencken', 'It is the dull man who is always sure, AND the sure man who is always dull.');
+
+INSERT INTO `fam_quotes` VALUES (47, 'H.L. Mencken', 'Philosophy consists very largely of one philosopher arguing that all others are jackasses. He usually proves it, AND I should add that he also usually proves that he is one himself.');
+
+INSERT INTO `fam_quotes` VALUES (48, 'H.L. Mencken', 'The chief value of money lies IN the fact that one lives IN a world IN which it is overestimated.');
+
+INSERT INTO `fam_quotes` VALUES (49, 'H.L. Mencken', 'The government consists of a gang of men exactly like you AND me. They have, taking one with another, no special talent for the business of government they have only a talent for getting AND holding office.');
+
+INSERT INTO `fam_quotes` VALUES (50, 'H.L. Mencken', 'Platitude an idea (a) that is admitted to be true by everyone, AND (b) that is not true.');
+
+INSERT INTO `fam_quotes` VALUES (51, 'H.L. Mencken', 'The capacity of human beings to bore one another seems to be vastly greater than that of any other animal.');
+
+INSERT INTO `fam_quotes` VALUES (52, 'H.L. Mencken', 'Say what you will about the Ten Commandments, you must always come back to the pleasant fact that there are only ten of them.');
+
+INSERT INTO `fam_quotes` VALUES (53, 'H.L. Mencken', 'Never let your inferiors do you a favor - it will be extremely costly.');
+
+INSERT INTO `fam_quotes` VALUES (54, 'H.L. Mencken', 'Puritanism The haunting fear that someone, somewhere, may be happy.');
+
+INSERT INTO `fam_quotes` VALUES (55, 'H.L. Mencken', 'The most common of all follies is to believe passionately IN the palpably not true. It is the chief occupation of mankind.');
+
+INSERT INTO `fam_quotes` VALUES (56, 'H.L. Mencken', 'Nobody ever went broke underestimating the taste of the American public.');
+
+INSERT INTO `fam_quotes` VALUES (57, 'H.L. Mencken', 'The difference BETWEEN a moral man AND a man of honor is that the latter regrets a discreditable act, even WHEN it has worked AND he has not been caught.');
+
+INSERT INTO `fam_quotes` VALUES (58, 'H.L. Mencken', 'We are here AND it is now. Further than that all human knowledge is moonshine.');
+
+INSERT INTO `fam_quotes` VALUES (59, 'H.L. Mencken', 'The older I grow the more I distrust the familiar doctrine that age brings wisdom.');
+
+INSERT INTO `fam_quotes` VALUES (60, 'H.L. Mencken', 'To die for an idea it is unquestionably noble. But how much nobler it would be if men died for ideas that were true');
+
+INSERT INTO `fam_quotes` VALUES (61, 'H.L. Mencken', 'The trouble with fighting for human freedom is that one spends most of one\'s time defending scoundrels. For it is against scoundrels that oppressive laws are first aimed, AND oppression must be stopped at the beginning if it is to be stopped at all.');
+
+INSERT INTO `fam_quotes` VALUES (62, 'H.L. Mencken', 'The world always makes the assumption that the exposure of an error is identical with the discovery of truth--that the error AND truth are simply opposite. They are nothing of the sort. What the world turns to, WHEN it is cured
+ON one error, is usually simply another error, AND maybe one worse than the first one.');
+
+INSERT INTO `fam_quotes` VALUES (63, 'H.L. Mencken', 'Under democracy one party always devotes its chief energies to trying to prove that the other party is unfit to rule - AND both commonly succeed, AND are right.');
+
+INSERT INTO `fam_quotes` VALUES (64, 'H.L. Mencken', 'Unquestionably, there is progress. The average American now pays out twice AS much IN taxes AS he formerly got IN wages.');
+
+INSERT INTO `fam_quotes` VALUES (65, 'H.L. Mencken', 'After all, all he did was string together a lot of old, well-known quotations.');
+
+INSERT INTO `fam_quotes` VALUES (66, 'H.L. Mencken', 'For every human problem, there is a neat, simple solution AND it is always wrong');
+
+INSERT INTO `fam_quotes` VALUES (67, 'H.L. Mencken', 'Conscience is the inner voice that warns us somebody may be looking.');
+
+INSERT INTO `fam_quotes` VALUES (68, 'Bernard Baruch', 'Don\'t try to buy at the bottom AND sell at the top. It can\'t be done except by liars.');
+
+INSERT INTO `fam_quotes` VALUES (69, 'Bernard Baruch', 'I made my money by selling too soon.');
+
+INSERT INTO `fam_quotes` VALUES (70, 'Bernard Baruch', 'To me, old age is always 15 years older than I am.');
+
+INSERT INTO `fam_quotes` VALUES (71, 'Bernard Baruch', 'Only AS you do know yourself can your brain serve you AS a sharp AND efficient tool. Know your own failings, passions, AND prejudices so you can separate them
+FROM what you see.');
+
+INSERT INTO `fam_quotes` VALUES (72, 'Bernard Baruch', 'Approach each new problem not with a view of finding what you hope will be there, but to get the truth, the realities that must be grappled with. You may not like what you find. IN that case you are entitled to try to change it. But do not deceive yourself AS to what you do find to be the facts of the situation.');
+
+INSERT INTO `fam_quotes` VALUES (73, 'Baha\'u\'llah', 'It is not for him to pride himself who loveth his own country, but rather for him who loveth the whole world. The earth is but one country AND mankind its citizens.');
+
+INSERT INTO `fam_quotes` VALUES (74, 'Upton Sinclair', 'Fascism is capitalism plus murder.');
+
+INSERT INTO `fam_quotes` VALUES (75, 'Upton Sinclair', 'I aimed at the public\'s heart, AND by accident I hit it IN the stomach.');
+
+INSERT INTO `fam_quotes` VALUES (76, 'Harry Emerson Fosdick', 'Don\'t simply retire
+FROM something have something to retire to.');
+
+INSERT INTO `fam_quotes` VALUES (77, 'Harry Emerson Fosdick', 'The fact that astronomies change while the stars abide is a true analogy of every realm of human life AND thought, religion not least of all. No existent theology can be a final formulation of spiritual truth.');
+
+INSERT INTO `fam_quotes` VALUES (78, 'Harry Emerson Fosdick', 'Hating people is like burning down your house to get rid of a rat.');
+
+INSERT INTO `fam_quotes` VALUES (79, 'Harry Emerson Fosdick', 'He who chooses the beginning of the road chooses the place it leads to. It is the means that determines the end.');
+
+INSERT INTO `fam_quotes` VALUES (80, 'Harry Emerson Fosdick', 'No horse gets anywhere until he is harnessed. No stream or gas drives anything until it is confined. No Niagara is ever turned into light AND power until it is tunneled. No life ever grows great until it is focused, dedicated, disciplined.');
+
+INSERT INTO `fam_quotes` VALUES (81, 'Harry Emerson Fosdick', 'Watch what people are cynical about, AND one can often discover what they lack.');
+
+INSERT INTO `fam_quotes` VALUES (82, 'Harry Emerson Fosdick', 'No steam or gas drives anything until it is confined. No life ever grows great until it is focused, dedicated, disciplined.');
+
+INSERT INTO `fam_quotes` VALUES (83, 'Harry Emerson Fosdick', 'Bitterness imprisons life love releases it. Bitterness paralyzes life love empowers it. Bitterness sours life love sweetens it. Bitterness sickens life love heals it. Bitterness blinds life love anoints its eyes.');
+
+INSERT INTO `fam_quotes` VALUES (84, 'Harry Emerson Fosdick', 'I would rather live IN a world
+WHERE my life is surrounded by mystery than live IN a world so small that my mind could comprehend it.'); 
+
+INSERT INTO `fam_quotes` VALUES (85, 'Harry Emerson Fosdick', 'The world is moving so fast these days that the man who says it can\'t be done is generally interrupted by someone doing it.');
+
+INSERT INTO `fam_quotes` VALUES (86, 'Harry Emerson Fosdick', 'The steady discipline of intimate friendship with Jesus results IN men becoming like Him.');
+
+INSERT INTO `fam_quotes` VALUES (87, 'Harry Emerson Fosdick', 'Life is a library owned by an author. It has a few books which he wrote himself, but most of them were written for him.');
+
+INSERT INTO `fam_quotes` VALUES (88, 'Harry Emerson Fosdick', 'Hold a picture of yourself long AND steadily enough IN your mind\'s eye, AND you will be drawn toward it.');
+
+INSERT INTO `fam_quotes` VALUES (89, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Barbossa For too long I\'ve been parched of thirst AND unable to quench it. Too long I\'ve been starving to death AND haven\'t died. I feel nothing. Not the wind
+ON my face nor the spray of the sea. Nor the warmth of a woman\'s flesh. You best start believing IN ghost stories Miss Turner. You\'re IN one.');
+
+INSERT INTO `fam_quotes` VALUES (90, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Jack Me I\'m dishonest, AND a dishonest man you can always trust to be dishonest. Honestly. It\'s the honest ones you want to watch out for, because you can never predict WHEN they\'re going to do something incredibly... stupid.');
+
+INSERT INTO `fam_quotes` VALUES (91, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Jack You seem somewhat familiar. Have I threatened you before');
+
+INSERT INTO `fam_quotes` VALUES (92, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Will Where\'s Elizabeth Jack She\'s safe, just like I promised. She\'s all
+
+SET to marry Norrington, just like she promised. AND you get to die for her, just like you promised. So we\'re all men of our word really... except for Elizabeth, who is IN fact, a woman.');
+
+INSERT INTO `fam_quotes` VALUES (93, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Jack WHEN you marooned me
+ON that god forsaken spit of land, you forgot one very important thing, mate I\'m Captain Jack Sparrow.');
+
+INSERT INTO `fam_quotes` VALUES (94, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Will We\'re going to steal a ship That ship Jack Commandeer. We\'re going to commandeer that ship. Nautical term.');
+
+INSERT INTO `fam_quotes` VALUES (95, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Jacoby I\'m gonna teach you the meaning of pain. Elizabeth You like pain Try wearing a corset.');
+
+INSERT INTO `fam_quotes` VALUES (96, 'Pirates of the Caribbean The Curse of the Black Pearl', 'Jack You need to find yourself a girl mate. Or perhaps the reason you practice three hours a day is that you already found one, AND are otherwise incapable of wooing said strumpet. You\'re not a eunuch are you');
+
+INSERT INTO `fam_quotes` VALUES (97, 'H.R. Haldeman', 'Once the toothpaste is out of the tube, it\'s hard to get it back in');
+
+INSERT INTO `fam_quotes` VALUES (98, 'H.R. Haldeman', 'You can\'t put the toothpaste back IN the tube.');
+
+INSERT INTO `fam_quotes` VALUES (99, 'H.R. Haldeman', 'We are getting into semantics again. If we use words, there is a very grave danger they will be misinterpreted.');
+
+INSERT INTO `fam_quotes` VALUES (100, 'Andr Gide', 'The most important things to say are those which often I did not think necessary for me to say -- because they were too obvious.'); 
